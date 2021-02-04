d62a61f503b73f9b66f1bfe03b2e6405f794276d
sanketjain <sanket.jain@columbia.edu>
2021-02-04 09:01:04 +0530

    Add method to handle new lines between numbers

--- CalculatorApplication/src/com/sanket/code/Calculator.java
+++ CalculatorApplication/src/com/sanket/code/Calculator.java
@@ -1,17 +1,14 @@
 package com.sanket.code;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class Calculator {
 
 	public static int Add(String numbers) {
 		if (numbers.equals(""))
 			return 0;
 
+		numbers = numbers.replaceAll("\\n", ",");
 		String[] nums = numbers.split(",");
 		int sum = 0;
-		int current = 0;
 		for (int index = 0; index < nums.length; index++) {
 			sum += Integer.parseInt(nums[index]);
 		}
