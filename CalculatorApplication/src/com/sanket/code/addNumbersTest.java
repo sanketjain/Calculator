9cd45dfa6df3d4ec0b76bf42ed40fe63f033e2d7
sanketjain <sanket.jain@columbia.edu>
2021-02-04 08:41:08 +0530

    Add two numbers

--- CalculatorApplication/src/com/sanket/code/addNumbersTest.java
+++ CalculatorApplication/src/com/sanket/code/addNumbersTest.java
@@ -10,8 +10,20 @@ public class addNumbersTest {
 	public void test() {
 		Integer output = Calculator.Add("");
 		assertEquals(0, output);
+		
 		Integer output1 = Calculator.Add("1");
 		assertEquals(1, output1);
+		
+		String input = "" + Integer.MAX_VALUE;
+		Integer outputMaxValue = Calculator.Add(input);
+		assertEquals(Integer.MAX_VALUE, outputMaxValue);
+		
+		String inputMin = "" + Integer.MIN_VALUE;
+		Integer outputMinValue = Calculator.Add(inputMin);
+		assertEquals(Integer.MIN_VALUE, outputMinValue);
+		
+		Integer outputOfTwoInputs = Calculator.Add("1,2");
+		assertEquals(3, outputOfTwoInputs);
 	}
 
 }
