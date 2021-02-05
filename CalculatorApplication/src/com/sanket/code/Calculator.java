fdc8494b78a33d65088c93214e06b733d4531a3c
sanketjain <sanket.jain@columbia.edu>
2021-02-05 09:49:51 +0530

    Use delimiter

--- CalculatorApplication/src/com/sanket/code/Calculator.java
+++ CalculatorApplication/src/com/sanket/code/Calculator.java
@@ -1,13 +1,21 @@
 package com.sanket.code;
 
+//https://osherove.com/tdd-kata-1
 public class Calculator {
 
 	public static int Add(String numbers) {
 		if (numbers.equals(""))
 			return 0;
 
-		numbers = numbers.replaceAll("\\n", ",");
-		String[] nums = numbers.split(",");
+		numbers = "//;\n1;2";
+		String delimiter = ",";
+		if (numbers.startsWith("//")) {
+			delimiter = numbers.substring(numbers.indexOf("//")+2, numbers.indexOf("\n"));
+			numbers = numbers.substring(numbers.indexOf("\n")+1);
+		}
+		
+		numbers = numbers.replaceAll("\\n", delimiter);
+		String[] nums = numbers.split(delimiter);
 		int sum = 0;
 		for (int index = 0; index < nums.length; index++) {
 			sum += Integer.parseInt(nums[index]);
--- CalculatorApplication/src/com/sanket/code/addNumbersTest.java
+++ CalculatorApplication/src/com/sanket/code/addNumbersTest.java
@@ -2,37 +2,37 @@ package com.sanket.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Random;
-
 import org.junit.jupiter.api.Test;
 
 public class addNumbersTest {
 
 	@Test
 	public void test() {
-//		Integer output = Calculator.Add("");
-//		assertEquals(0, output);
-//
-//		Integer output1 = Calculator.Add("1");
-//		assertEquals(1, output1);
-//
-//		String input = "" + Integer.MAX_VALUE;
-//		Integer outputMaxValue = Calculator.Add(input);
-//		assertEquals(Integer.MAX_VALUE, outputMaxValue);
-//
-//		String inputMin = "" + Integer.MIN_VALUE;
-//		Integer outputMinValue = Calculator.Add(inputMin);
-//		assertEquals(Integer.MIN_VALUE, outputMinValue);
-//
-//		Integer outputOfTwoInputs = Calculator.Add("1,2");
-//		assertEquals(3, outputOfTwoInputs);
-//
-//		Integer outPutOfUnknownAmountOfNumbers = Calculator.Add("1,2,3,4,2,5,3,4");
-//		assertEquals(24, outPutOfUnknownAmountOfNumbers);
+		Integer output = Calculator.Add("");
+		assertEquals(0, output);
+
+		Integer output1 = Calculator.Add("1");
+		assertEquals(1, output1);
+
+		String input = "" + Integer.MAX_VALUE;
+		Integer outputMaxValue = Calculator.Add(input);
+		assertEquals(Integer.MAX_VALUE, outputMaxValue);
+
+		String inputMin = "" + Integer.MIN_VALUE;
+		Integer outputMinValue = Calculator.Add(inputMin);
+		assertEquals(Integer.MIN_VALUE, outputMinValue);
+
+		Integer outputOfTwoInputs = Calculator.Add("1,2");
+		assertEquals(3, outputOfTwoInputs);
+
+		Integer outPutOfUnknownAmountOfNumbers = Calculator.Add("1,2,3,4,2,5,3,4");
+		assertEquals(24, outPutOfUnknownAmountOfNumbers);
 
 		Integer outputOfSlashN = Calculator.Add("1\n2,3");
 		assertEquals(6, outputOfSlashN);
-
+		
+		Integer outputDelimiter = Calculator.Add("//;\n1;2");
+		assertEquals(3, outputDelimiter);
 	}
 
 }
